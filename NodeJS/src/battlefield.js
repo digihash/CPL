e73fe5624a89d171acb504c5e5c2e34e8471555b
Kevin Holvoet <kevin@holvoet.eu>
2013-12-27 17:17:04 +0100

    Fixed tests except for rockets and the API

--- NodeJS/src/battlefield.js
+++ NodeJS/src/battlefield.js
@@ -31,6 +31,8 @@
 		else {
 			parse_fieldprocessors(this.configuration, this.field, fieldprocessors);
 		}
+
+		this.tanks = [];
 	};
 
 	var add_wall_around_field = function(config, field) {
@@ -57,11 +59,11 @@
 		return [posheight, poswidth];
 	};
 
-	Battlefield.prototype.tanks = new Array();
 	Battlefield.prototype.addTank = function(tank) {
 		this.tanks.push(tank);
 		this.field[tank.position[0]][tank.position[1]] = Tile.TANK;
 	};
+
 	Battlefield.prototype.removeTank = function(tank) {
 		var index = this.tanks.indexOf(tank);
 		if (index > -1) {
@@ -94,13 +96,59 @@
 			}
 		})();
 
-		if( (newPos[0] >= 0 && newPos[1] >= 0) && this.field[newPos[0]][newPos[1]] === Tile.FREE) {
+		if( (newPos[0] >= 0 && newPos[1] >= 0) && (newPos[0] < this.configuration.height && newPos[1] < this.configuration.width) && this.field[newPos[0]][newPos[1]] === Tile.FREE) {
 			this.field[tank.position[0]][tank.position[1]] = Tile.FREE;
 			tank.position = newPos;
 			this.field[newPos[0]][newPos[1]] = Tile.TANK;
 		}
 		return tank.position;
 	};
+
+	Battlefield.prototype.scope = function(pos) {
+		if(this.configuration.height === 1 && this.configuration.width === 1 && pos[0] === 0 && pos[1] === 0)
+			return [[0]];
+
+		if(this.configuration.height <= pos[0] || this.configuration.width <= pos[1])
+			throw "incorrect scope";
+
+		var heightMax = ((pos[0] + this.configuration.scope) < this.configuration.height) ? pos[0]+this.configuration.scope : this.configuration.height-1;
+		var heightMin = ((pos[0] - this.configuration.scope) >= 0) ? pos[0] - this.configuration.scope : 0;
+		var widthMax = ((pos[1] + this.configuration.scope) < this.configuration.width) ? pos[1] + this.configuration.scope : this.configuration.width-1;
+		var widthMin = ((pos[1] - this.configuration.scope) >= 0) ? pos[1] - this.configuration.scope : 0;
+
+		var newScope = [];
+		var i, j, x, y;
+		for (i = heightMin, x = 0; i <= heightMax; i++, x++) {
+			newScope[x] = [];
+			for (j = widthMin, y = 0; j <= widthMax; j++, y++) {
+				newScope[x][y] = this.field[i][j];
+			}
+		}
+		return newScope;
+
+	};
+
+	Battlefield.prototype.tanksInScope = function(tank) {
+		var heightMax = ((tank.position[0] + this.configuration.scope) < this.configuration.height) ? tank.position[0]+this.configuration.scope : this.configuration.height-1;
+		var heightMin = ((tank.position[0] - this.configuration.scope) >= 0) ? tank.position[0] - this.configuration.scope : 0;
+		var widthMax = ((tank.position[1] + this.configuration.scope) < this.configuration.width) ? tank.position[1] + this.configuration.scope : this.configuration.width-1;
+		var widthMin = ((tank.position[1] - this.configuration.scope) >= 0) ? tank.position[1] - this.configuration.scope : 0;
+
+		var toReturnTanks = [];
+		var i, j;
+		for (i = heightMin; i <= heightMax; i++) {
+			for (j = widthMin; j <= widthMax; j++) {
+				if( !(tank.position[0] === i && tank.position[1] === j) && this.field[i][j] === Tile.TANK) {
+					for (var x = 0; x < this.tanks.length; x++) {
+						if(this.tanks[x].position[0] === i && this.tanks[x].position[1] === j) {
+							toReturnTanks.push(this.tanks[x]);
+						}
+					}
+				}
+			}
+		}
+		return toReturnTanks;
+	}
 
 	module.exports.Battlefield = Battlefield;
 	module.exports.Tile = Tile;
